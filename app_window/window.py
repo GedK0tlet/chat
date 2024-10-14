a46b506d9f80178497d8ca4d7aceda5a488f1f3b
Ged <53939907+Ged-sudo@users.noreply.github.com>
2024-10-14 22:13:13 +0300

    some changes

--- app_window/window.py
+++ app_window/window.py
@@ -1,25 +1,18 @@
 from tkinter import *
 
 def get_text():
-    # print(text_box.get(1.0, END))
     label_sended["text"] += text_box.get(1.0, END) + "\n"
     text_box.delete(1.0, END)
 
 window = Tk()
 window.geometry('700x700')
 
-canvas = Canvas(window, width=500, height=700)
 text_box = Text(window, font=('Arial', 15), wrap='word', height=5)
 button_send = Button(window, text='Send', command=get_text)
-label_sended = Label(canvas, text='', height=15, font=('Arial', 15))
-
-scrollbar = Scrollbar(canvas, orient=VERTICAL, command=canvas.yview)
-scrollbar.place(relx=1, rely=0, relheight=1, anchor=NE)
-canvas.config(yscrollcommand=scrollbar.set, scrollregion=(0, 0, 0, 0))
+label_sended = Label(window, text='', height=15, font=('Arial', 15))
 
 label_sended.pack()
 text_box.pack()
 button_send.pack()
 
 window.mainloop()
-https://github.com/GedK0tlet/chat
